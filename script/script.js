ed8ac48f8ae54b945ceae7ed1c15fe845c38cac6
Maria Veronica Ortiz <mariatua2@gmail.com>
2017-12-05 22:34:07 -0400

    Adding phrases and random compliments

--- script/script.js
+++ script/script.js
@@ -1,11 +1,39 @@
 
 
-window.onload = function startTime() {
+window.onload = function(){
+  startTime();
+  phrase();
+  randomCompliment();
+}
+//declaring variables
+var dateObj = new Date();
+var internalhour = dateObj.getHours();
+
+//phrase below the time
+function phrase() {
+  if (internalhour>=0 && internalhour<12) {
+      $("#phrase").append("Good Morning, ");
+}
+
+else if (internalhour>=12 && internalhour<17) {
+      $("#phrase").append("Good Afternoon, ");
+}
+
+else {
+      $("#phrase").append("Good Evening, ");
+}
+}
+
+
+function startTime() {
   var dateObj = new Date();
-
-  var h = dateObj.toLocaleString('en-US', { hour: 'numeric',minute:'numeric', hour12: true });
-
+  var h = dateObj.toLocaleString('en-US', { hour: 'numeric',minute:'numeric', second: 'numeric', hour12: true });
   document.getElementById("clock").innerHTML= h;
-
   var t = setTimeout(startTime, 1000); //this calls the fuction after every second
+}
+
+function randomCompliment () {
+  var arrayOfCompliments = ["handsome", "cutie", "human", "pal", "smart", "sexy"];
+  var randomCompliment = Math.floor(Math.random()*arrayOfCompliments.length);
+  $("#phrase").append(arrayOfCompliments[randomCompliment]);
 }
