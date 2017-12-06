18e54d48207a9d462bfccf10803094555108573b
Maria Veronica Ortiz <mariatua2@gmail.com>
2017-12-05 20:39:04 -0400

    adding setTimeOut

--- script/script.js
+++ script/script.js
@@ -1,10 +1,11 @@
 
 
-window.onload = function() {
+window.onload = function startTime() {
   var dateObj = new Date();
 
   var h = dateObj.toLocaleString('en-US', { hour: 'numeric',minute:'numeric', hour12: true });
 
   document.getElementById("clock").innerHTML= h;
 
+  var t = setTimeout(startTime, 1000); //this calls the fuction after every second
 }
