eb286141836dd80a26f2d18a2b5c02b4c29aa421
lgainsbrook <lauren.gainsbrook@gmail.com>
2017-12-12 17:24:47 -0500

    add todo list app

--- script/script.js
+++ script/script.js
@@ -66,3 +66,53 @@ function getMainFocus() {
     var mainFocus = document.getElementById('mainFocusQuestion').value;
     document.getElementById('mainFocusParagraph').innerHTML = mainFocus
 }
+
+//TO DO LIST//////////////////////////////////////////////////////////////////////////////////////
+
+var todoList = {
+    todos: [{todoText: "task 1", completed: true}, {todoText: "task 2", completed: true}, {todoText: "task 3", completed:true}],
+    displayTodos: function() {
+        console.clear();
+        for (i = 0; i < this.todos.length; i++) {
+            if (this.todos[i].completed === true) {
+                console.log("(x) " + this.todos[i].todoText);
+            } else {
+                console.log("( ) " + this.todos[i].todoText);
+            }
+        }
+    },
+    addTodo: function(todoText) {
+        this.todos.push({
+            todoText: todoText,
+            completed: false
+        });
+        this.displayTodos();
+    },
+    changeTodo: function(index, todoText) {
+        this.todos[index].todoText = todoText;
+        this.displayTodos();
+    },
+    deleteTodo: function(index) {
+        this.todos.splice(this.todos(index, 1));
+        this.displayTodos();
+    },
+    toggleCompleted: function(index) {
+        this.todos[index].completed = !this.todos[index].completed;
+        this.displayTodos();
+    },
+    toggleAll: function() {
+        var all = true;
+        for (i=0; i<this.todos.length; i++) {
+            if (this.todos[i].completed === false) {
+                this.todos[i].completed = true;
+                all = false;
+            }
+        }
+        if (all === true) {
+            for (i=0; i<this.todos.length; i++) {
+                this.todos[i].completed = false;
+            }
+        }
+        this.displayTodos();
+    }
+};
