8025dbdbe5038a4d166e09fab532afa876c2c29a
Fouad <fmtabbara@hotmail.co.uk>
2017-12-14 12:58:50 +0000

    first commit for quote generator

--- script/script.js
+++ script/script.js
@@ -1,97 +1,24 @@
 
 
 window.onload = function(){
-  startTime();
-  phrase();
-  randomCompliment();
-  settingMenu();
-}
-//declaring variables
-var dateObj = new Date();
-var internalhour = dateObj.getHours();
-
-//phrase below the time
-function phrase() {
-  if (internalhour>=0 && internalhour<12) {
-      $("#phrase").append("Good Morning, ");
+  quoteGenerator()
 }
 
-else if (internalhour>=12 && internalhour<17) {
-      $("#phrase").append("Good Afternoon, ");
-}
+function quoteGenerator(){
+  $.get('https://quotes.rest/qod.json', function(responseText) {
+      formatter(responseText);
+  });
 
-else {
-      $("#phrase").append("Good Evening, ");
-}
-}
-
-
-function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;}
-
-// the clock function
-function startTime() {
-  var dateObj = new Date();
-
-if ($('#timeCheck').is(':checked') === true)
-
-{
-  var getHours = dateObj.getHours();
-  var getMinutes = dateObj.getMinutes();
-  h= checkTime(getHours);
-  m = checkTime(getMinutes);
-  document.getElementById("clock").innerHTML= h + ":" + m;
-  var t = setTimeout(startTime, 1000); //this calls the fuction after every second
-
-  //check and uncheck the radio button
-  $(':radio').mousedown(function(e){
-  var $self = $(this);
-  if( $self.is(':checked') ){
-    var uncheck = function(){
-      setTimeout(function(){$self.removeAttr('checked');},0);
-    };
-    var unbind = function(){
-      $self.unbind('mouseup',up);
-    };
-    var up = function(){
-      uncheck();
-      unbind();
-    };
-    $self.bind('mouseup',up);
-    $self.one('mouseout', unbind);
+  function formatter(response){
+    let quote = JSON.stringify(response.contents.quotes[0].quote);
+    let author = JSON.stringify(response.contents.quotes[0].author);
+    let regEx = /[a-z]|\s/gi;
+    author = author.match(regEx);
+    render(quote,author);
   }
-});
-}
 
-else  {
-
-  var h = dateObj.toLocaleString('en-US', { hour: 'numeric',minute:'numeric', hour12: true });
-  document.getElementById("clock").innerHTML= h;
-  var t = setTimeout(startTime, 1000); //this calls the fuction after every second
-}
-}
-
-//Compliments next to phrase
-function randomCompliment () {
-  var arrayOfCompliments = ["handsome.", "cutie.", "human.", "pal.", "smart.", "sexy.","classy."];
-  var randomCompliment = Math.floor(Math.random()*arrayOfCompliments.length);
-  $("#phrase").append(arrayOfCompliments[randomCompliment]);
-}
-
-function settingMenu() {
-
-$( ".cross" ).hide();
-$( ".menu" ).hide();
-$( ".setting" ).click(function() {
-$( ".menu" ).toggle( "slide", function() {
-$( ".cross" ).show();
-});
-});
-
-$( ".cross" ).click(function() {
-$( ".menu" ).toggle( "slide", function() {
-$( ".cross" ).hide();
-});
-});
+  function render(output,author){
+    $(".quote").html(output);
+    $(".author").html(author);
+  }
 }
