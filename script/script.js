73e2ed4c1d5f4ecb8e4924f2574b29224bc29138
Fouad <fmtabbara@hotmail.co.uk>
2018-01-11 23:09:17 +0000

    changed static image set to random image from unsplashed website

--- script/script.js
+++ script/script.js
@@ -1,10 +1,6 @@
-let images = ['beach-1.jpg','mountain-1.jpg','beach-2.jpg','mountain-2.jpg','skyline-1.jpeg','window-1.jpg','lake-1.jpg','desert-1.jpg','boats-1.jpg','toucan-1.jpg'];
+$(document).ready(function(){
+	let imageURL = 'https://source.unsplash.com/1900x768/?nature,skyline,beach';	
+	$('#background-container').css('background-image', 'url('+ imageURL +')');
+	$('#background-container').animate({ opacity: 0.6 }, { duration: 3000 });
 
-window.onload = function(){
-	let max = images.length-1;
-	let min = 1 ;
-	let random = Math.floor(Math.random()*(max-min+1)+min);
-			
-	$('#background-container').css('background-image', 'url('+ images[random] +')');
-	$('#background-container').animate({ opacity: 0.6 }, { duration: 1000 });
-}
+});
