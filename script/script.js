b31a7b29e86e0b5acb528ab99e0788b25b11d757
Fouad <fmtabbara@hotmail.co.uk>
2018-01-13 00:30:04 +0000

    addition of show-off menu and various positioning tweaks

--- script/script.js
+++ script/script.js
@@ -25,37 +25,38 @@ function backgroundLoader(){
 
 //SHOWOFF ELEMENT/////////////////////////////////////////////////////////////////////////////////
 
-  let sOFlag = true;
-      $("#toucan-logo").click(function(){
-        if(sOFlag===true){
-          $("#toucan-logo").addClass("spin-icon");
-          $("#toucan-logo").css("opacity","1");
-          $("#showoff-container").addClass("open-container");
-          $("#showoff-container").removeClass("close-container");
-          setTimeout(function(){
-            $("#showoff-text").css("display","grid");
-          },300);
-          setTimeout(function(){
-            $("#toucan-logo").removeClass("spin-icon");
-            $("#showoff-text").css("display","grid");
-            sOFlag = false;
-          },1000);
-        }
-        else{
-              $("#showoff-container").removeClass("open-container");
-              $("#showoff-container").addClass("close-container");
-              $("#toucan-logo").addClass("spin-icon-reverse");
-              setTimeout(function(){
-                $("#showoff-text").css("display","none");
-              },150);
-              setTimeout(function(){
-                $("#toucan-logo").removeClass("spin-icon-reverse");
-                $("#showoff-text").css("display","none");
-                $("#toucan-logo").css("opacity","0.6");
-                sOFlag = true;
-              },800);
-        }
-      })
+  let showOffFlag = true;
+
+function openToucanMenu(){
+  if(showOffFlag===true){
+    $("#toucan-logo").addClass("spin-icon");
+    $("#toucan-logo").css("opacity","1");
+    $("#showoff-container").addClass("open-container");
+    $("#showoff-container").removeClass("close-container");
+    setTimeout(function(){
+      $("#showoff-text").css("display","grid");
+    },300);
+    setTimeout(function(){
+      $("#toucan-logo").removeClass("spin-icon");
+      $("#showoff-text").css("display","grid");
+      showOffFlag = false;
+    },1000);
+  }
+  else{
+        $("#showoff-container").removeClass("open-container");
+        $("#showoff-container").addClass("close-container");
+        $("#toucan-logo").addClass("spin-icon-reverse");
+        setTimeout(function(){
+          $("#showoff-text").css("display","none");
+        },150);
+        setTimeout(function(){
+          $("#toucan-logo").removeClass("spin-icon-reverse");
+          $("#showoff-text").css("display","none");
+          $("#toucan-logo").css("opacity","0.2");
+          showOffFlag = true;
+        },800);
+  }
+}
 
 
 
